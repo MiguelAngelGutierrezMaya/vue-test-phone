c2e5706b04f7239efc5d9c2b80497e1fccc58586
Miguel Angel <miguel.gutierrez@kbe.ai>
2023-08-04 02:04:06 -0500

    master | Ajustes en la libreria

--- src/install.js
+++ src/install.js
@@ -1,16 +1,16 @@
 import App from './App.vue';
 
-const PhoneField = {
+const VuePhoneField = {
   install(Vue) {
     // Let's register our component globally
     // https://vuejs.org/v2/guide/components-registration.html
-    Vue.component('phone-field', App);
+    Vue.component('vue-phone-field', App);
   },
 };
 
 // Automatic installation if Vue has been added to the global scope.
 if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(PhoneField);
+  window.Vue.use(VuePhoneField);
 }
 
-export default PhoneField;
+export default VuePhoneField;
