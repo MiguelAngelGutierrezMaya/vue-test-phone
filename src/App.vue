04913241bc12163079c077a8bd25479d541176a5
Miguel Angel <miguel.gutierrez@kbe.ai>
2023-08-04 00:59:45 -0500

    master | Adding fixes to component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    class="KBPhone"
+    :class="{ 'KBPhone--open': countryPicker }"
+    :key="rerender"
+  >
+    <div
+      class="KBPhone__countries newBackground inverted"
+      v-if="invert"
+      :class="{ 'KBPhone__countries--open': countryPicker }"
+    >
+      <div class="search userDetails">
+        <!-- No usar traducciones en componentes -->
+        <!-- <input type="text" v-model="search" :placeholder="$t('search')" />  -->
+        <input type="text" v-model="search" :placeholder="'Buscar'" />
+      </div>
+      <div class="list newBackground">
+        <div
+          v-for="(country, index) in countriesComputed"
+          :key="index"
+          class="country"
+          @click="select(country)"
+        >
+          <img class="iti-flag" :class="country.code" />
+          <span>(+{{ country.number }}) {{ country.country }}</span>
+        </div>
+      </div>
+    </div>
+    <div
+      class="KBPhone__container"
+      :class="{
+        'KBPhone__container--incorrect': !valid,
+        'KBPhone__container--outlined': outlined,
+        'KBPhone__container--outlined-incorrect': outlined && !valid,
+        'KBPhone__container--seamless': seamless,
+        'KBPhone__container--seamless-incorrect': seamless && !valid,
+        'KBPhone__container--focused': focused,
+      }"
+    >
+      <img
+        class="iti-flag"
+        :class="`${outlined ? '-ml-4 mr-3' : ''} ${phoneNumber.code}`"
+        @click="countryPicker = !countryPicker"
+      />
+      <span class="cursor-default">(+{{ phoneNumber.number }})</span>
+      <input
+        @paste="paste($event)"
+        type="number"
+        ref="phoneInput"
+        v-model="phoneNumber.phone"
+        :placeholder="placeholder"
+        :disabled="disabled"
+        @input="input"
+        :autofocus="autofocus"
+        @keyup.enter="save"
+        @focus="focused = true"
+        @blur="focused = false"
+      />
+    </div>
+    <div
+      class="KBPhone__countries newBackground"
+      v-if="!invert"
+      :class="{ 'KBPhone__countries--open': countryPicker }"
+    >
+      <div class="search userDetails">
+        <!-- No usar traducciones -->
+        <!-- <input type="text" v-model="search" :placeholder="$t('search')" /> -->
+        <input type="text" v-model="search" :placeholder="'Search'" />
+      </div>
+      <div class="list newBackground">
+        <div
+          v-for="(country, index) in countriesComputed"
+          :key="index"
+          class="country"
+          @click="select(country)"
+        >
+          <img class="iti-flag" :class="country.code" />
+          <span>(+{{ country.number }}) {{ country.country }}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script lang="ts">
+import App from './App';
+export default App;
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="scss">
+@import './App.scss';
 </style>
---
+++ src/utils/enum/SpecialCountries.ts
@@ -0,0 +1,8 @@
+export enum SpecialCountries {
+  Bahamanas = 'bahamas',
+  SintMaarten = 'sint maarten',
+  SaintLucia = 'saint lucia',
+  AmericanSamoa = 'american samoa',
+  Argentina = 'argentina',
+  Mexico = 'mexico (méxico)',
+}
